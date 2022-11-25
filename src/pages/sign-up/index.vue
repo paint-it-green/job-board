9349c0e983ce97cde512d1f2747e767c5ee29bf2
paint-it-green <gerwinbalhin@gmail.com>
2022-11-26 04:24:05 +0800

    sign up integration

---
+++ src/pages/sign-up/index.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useSignUpEmailPassword } from '@nhost/vue'
+import { useRouter } from 'vue-router'
+
+const { signUpEmailPassword } = useSignUpEmailPassword()
+const firstName = ref('')
+const lastName = ref('')
+const email = ref('')
+const password = ref('')
+
+const handleSubmit = async (event: Event) => {
+  event.preventDefault()
+  const { isError, error } = await signUpEmailPassword(email, password, {
+    metadata: { firstName, lastName },
+    redirectTo: '/sign-in',
+  })
+
+  if (isError)
+    console.log(error)
+}
+</script>
+
+<template>
+  <form @submit="handleSubmit">
+    <input v-model="firstName" placeholder="First name" class="input"><br>
+    <input v-model="lastName" placeholder="Last name" class="input"><br>
+    <input
+      v-model="email"
+      type="email"
+      placeholder="Email"
+      class="input"
+    ><br>
+    <input
+      v-model="password"
+      type="password"
+      placeholder="Password"
+      class="input"
+    ><br>
+
+    <button class="btn-submit" type="submit">
+      Sign up
+    </button>
+  </form>
+</template>
